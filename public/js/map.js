222e1b6ba91e8327e9d649b438dca9d41a2f9c35
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-10-07 17:47:53 +0530

    added error handling for maps not showing

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,17 @@
 maptilersdk.config.apiKey = mapToken;
 
-const map = new maptilersdk.Map({
-    container: "map", // container's id or the HTML element to render the map
-    style: maptilersdk.MapStyle.STREETS, // style URL
-    center: listing.geometry.coordinates, // starting position [lng, lat]
-    zoom: 11, // starting zoom
-});
+if (
+  listing &&
+  listing.geometry &&
+  Array.isArray(listing.geometry.coordinates) &&
+  listing.geometry.coordinates.length === 2
+) {
+  const map = new maptilersdk.Map({
+      container: "map", // container's id or the HTML element to render the map
+      style: maptilersdk.MapStyle.STREETS, // style URL
+      center: listing.geometry.coordinates, // starting position [lng, lat]
+      zoom: 11, // starting zoom
+  });
 
 new maptilersdk.Marker({ color: "red" })
   .setLngLat(listing.geometry.coordinates)
@@ -18,3 +24,25 @@ new maptilersdk.Marker({ color: "red" })
     `)
   )
   .addTo(map);
+}
+
+else {
+  const map = new maptilersdk.Map({
+      container: "map", // container's id or the HTML element to render the map
+      style: maptilersdk.MapStyle.STREETS, // style URL
+      center: [71,26], // starting position [lng, lat]
+      zoom: 11, // starting zoom
+  });
+
+new maptilersdk.Marker({ color: "red" })
+  .setLngLat([71,26])
+  .setPopup(
+    new maptilersdk.Popup().setHTML(`
+      <div>
+        <h5>${listing.title}</h5>
+        <p><i>Exact location shown after booking</i></p>
+      </div>
+    `)
+  )
+  .addTo(map);
+}
